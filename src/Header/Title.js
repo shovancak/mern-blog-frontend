2f1b45a9b1cfdb74dd5e0703b4480d7d9ecbfadd
shovancak <s.hovancak@gmail.com>
2020-07-30 21:44:36 +0200

    Styling changes

--- src/Header/Title.js
+++ src/Header/Title.js
@@ -3,5 +3,5 @@ import React from "react";
 import "./Title.css";
 
 export default function Title() {
-  return <div className="title">BLOGGIE</div>;
+  return <div className="title">M-E-R-N Stack</div>;
 }
