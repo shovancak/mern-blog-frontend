b0c27f4ac990658b7e5636e8da2c1db37cf5d1e2
shovancak <s.hovancak@gmail.com>
2020-07-30 14:34:24 +0200

    Basic implementation of SideDrawer for mobie navigations

--- src/Header/Navigation.js
+++ src/Header/Navigation.js
@@ -4,23 +4,31 @@ import Header from "./Header";
 import Title from "./Title";
 import { Link } from "react-router-dom";
 import NavigationLinks from "./NavigationLinks";
+import SideDrawer from "./SideDrawer";
 import "./Navigation.css";
 
 const Navigation = (props) => {
   return (
-    <Header>
-      <button className="nav-menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <Link to="/">
-        <Title />
-      </Link>
-      <nav>
-        <NavigationLinks />
-      </nav>
-    </Header>
+    <React.Fragment>
+      <SideDrawer>
+        <nav>
+          <NavigationLinks />
+        </nav>
+      </SideDrawer>
+      <Header>
+        <button className="nav-menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <Link to="/">
+          <Title />
+        </Link>
+        <nav>
+          <NavigationLinks />
+        </nav>
+      </Header>
+    </React.Fragment>
   );
 };
 
---
+++ src/Header/SideDrawer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SideDrawer = (props) => {
+  return <aside>{props.children}</aside>;
+};
+
+export default SideDrawer;
