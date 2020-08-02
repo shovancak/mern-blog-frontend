9be57b46229a37c68a19fc2153ab9dd79c6823a0
shovancak <s.hovancak@gmail.com>
2020-08-02 18:20:40 +0200

    Basic responsive styling

--- src/users/components/UserItem.js
+++ src/users/components/UserItem.js
@@ -6,15 +6,20 @@ const UserItem = (props) => {
   return (
     <div className="card">
       CARD
-      <div>
-        CONTENT
+      <div className="info-user">
+        INFO
         <div>AVATAR</div>
         <div>
-          <div>NAME: {props.name}</div>
+          <div>Name: {props.name}</div>
           <div>{`${props.amount} ${
             props.amount === 1 ? "Article" : "Articles"
           }`}</div>
         </div>
+      </div>
+      <div className="info-article">
+        LAST ARTICLE
+        <div>TITLE</div>
+        <div>DESCRIPTION</div>
       </div>
     </div>
   );
