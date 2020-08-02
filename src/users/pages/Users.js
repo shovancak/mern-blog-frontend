117bd7b2ad710d20514d7b93962649d039aa86a6
shovancak <s.hovancak@gmail.com>
2020-08-02 17:21:46 +0200

    Rendering list of users

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,7 +1,46 @@
 import React from "react";
 
+import UserList from "../components/UserList";
+
 const Users = () => {
-  return <div>USERS</div>;
+  const DUMMY_USERS = [
+    {
+      id: "u1",
+      name: "Samuel",
+      image: "https://i.imgur.com/DcylgJM.jpg",
+      amount: 2,
+    },
+    {
+      id: "u2",
+      name: "Max",
+      image: "https://i.imgur.com/DcylgJM.jpg",
+      amount: 1,
+    },
+    {
+      id: "u3",
+      name: "Oliver",
+      image: "https://i.imgur.com/DcylgJM.jpg",
+      amount: 1,
+    },
+    {
+      id: "u4",
+      name: "Andrei",
+      image: "https://i.imgur.com/DcylgJM.jpg",
+      amount: 3,
+    },
+    {
+      id: "u5",
+      name: "Yhua",
+      image: "https://i.imgur.com/DcylgJM.jpg",
+      amount: 4,
+    },
+  ];
+
+  return (
+    <div>
+      <UserList users={DUMMY_USERS} />
+    </div>
+  );
 };
 
 export default Users;
