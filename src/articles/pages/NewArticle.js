e5ebfba54e192667f6a2eb64a606dc84b6389c06
shovancak <s.hovancak@gmail.com>
2020-08-10 16:14:06 +0200

    Using Input component to create input formular for creating new place

--- src/articles/pages/NewArticle.js
+++ src/articles/pages/NewArticle.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import Input from "../../shared/components/Input";
+
+import "./NewArticle.css";
+
 const NewArticle = () => {
-  return <div>NEW ARTICLE</div>;
+  return (
+    <form className="article-form center">
+      <Input element="input" type="text" label="Title" />
+    </form>
+  );
 };
 
 export default NewArticle;
