27a5a3273052c4ada4864f4499774110584f093e
shovancak <s.hovancak@gmail.com>
2020-08-11 18:51:05 +0200

    Adding button for creating new article

--- src/articles/pages/components/ArticleList.js
+++ src/articles/pages/components/ArticleList.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import ArticleItem from "./ArticleItem";
 import Card from "../../../shared/components/Card";
+import Button from "../../../shared/components/Button";
 
 import "./ArticleList.css";
 
@@ -10,6 +11,9 @@ const ArticleList = (props) => {
     return (
       <Card>
         <h2>You have no articles created.</h2>
+        <Button to="/articles/new" className="button-na">
+          NEW ARTICLE
+        </Button>
       </Card>
     );
   }
