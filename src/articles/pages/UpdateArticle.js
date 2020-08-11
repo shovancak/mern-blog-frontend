c93275ce6faeaf39d4fbf92557a36f91355529b1
shovancak <s.hovancak@gmail.com>
2020-08-11 15:52:35 +0200

    Adding form for updating article + styling

--- src/articles/pages/UpdateArticle.js
+++ src/articles/pages/UpdateArticle.js
@@ -1,7 +1,109 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import { VALIDATOR_REQUIRE } from "../../shared/utilities/validators";
+
+import Card from "../../shared/components/Card";
+import Input from "../../shared/components/Input";
+import Button from "../../shared/components/Button";
+
+import "./UpdateArticle.css";
+
+const DUMMY_ARTICLES = [
+  {
+    id: "a1",
+    creator: "u1",
+    title: "MERN Stack",
+    imageUrl: "https://i.morioh.com/67feeaf72f.png",
+    description:
+      "One of the most popular technology stacks in web development.",
+    text:
+      "MERN Stack is a Javascript Stack that is used for easier and faster deployment of full-stack web applications. MERN Stack comprises of 4 technologies namely: MongoDB, Express, React and Node.js. It is designed to make the development process smoother and easier.",
+  },
+  {
+    id: "a2",
+    creator: "u2",
+    title: "React",
+    imageUrl: "https://probella.com/wp-content/uploads/2018/03/React-JS.png",
+    description:
+      "React JS is a JavaScript library used in web development to build interactive elements on websites.",
+    text:
+      "React is a JavaScript library that specializes in helping developers build user interfaces, or UIs.In terms of websites and web applications, UIs are the collection of on- screen menus, search bars, buttons, and anything else someone interacts with to USE a website or app.",
+  },
+  {
+    id: "a3",
+    creator: "u1",
+    title: "NodeJs",
+    imageUrl: "https://miro.medium.com/proxy/1*q9ww_u32hhpMaA-Q_s1ujw.png",
+    description:
+      "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.",
+    text:
+      "Node.js is a free, open-sourced, cross-platform JavaScript run-time environment that lets developers write command line tools and server-side scripts outside of a browser.",
+  },
+];
 
 const UpdateArticle = () => {
-  return <div>UPDATE ARTICLE</div>;
+  const articleId = useParams().articleId;
+
+  const articleToUpdate = DUMMY_ARTICLES.find(
+    (article) => article.id === articleId
+  );
+
+  if (!articleToUpdate) {
+    return (
+      <Card>
+        <h2>Article not found.</h2>
+      </Card>
+    );
+  }
+
+  return (
+    <form className="article-form center">
+      <div className="article-form-header">UPDATE ARTILCE</div>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a title for article"
+        onInput={() => {}}
+        value={articleToUpdate.title}
+        valid={true}
+      />
+      <Input
+        id="description"
+        label="Description"
+        rows="3"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a description for article"
+        onInput={() => {}}
+        value={articleToUpdate.description}
+        valid={true}
+      />
+      <Input
+        id="text"
+        label="Text"
+        rows="25"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a text for article"
+        onInput={() => {}}
+        value={articleToUpdate.text}
+        valid={true}
+      />
+      <Button
+        type="submit"
+        // disabled={!formState.isValid}
+        // className={`${
+        //   formState.isValid
+        //     ? `form-submit-button-valid`
+        //     : `form-submit-button-invalid`
+        // }`}
+      >
+        UPDATE
+      </Button>
+    </form>
+  );
 };
 
 export default UpdateArticle;
