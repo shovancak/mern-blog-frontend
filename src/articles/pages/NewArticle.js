4db2cef7e8e4661ab4273d2e695ec6143933f660
shovancak <s.hovancak@gmail.com>
2020-08-11 12:30:04 +0200

    Ussing useReducer hook for managing form validity

--- src/articles/pages/NewArticle.js
+++ src/articles/pages/NewArticle.js
@@ -1,14 +1,58 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useReducer } from "react";
 
 import Input from "../../shared/components/Input";
+import Button from "../../shared/components/Button";
 import { VALIDATOR_REQUIRE } from "../../shared/utilities/validators";
 
 import "./NewArticle.css";
 
-const NewArticle = () => {
-  const inputTitleHandler = useCallback((id, value, isValid) => {}, []);
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
 
-  const inputDescriptionHandler = useCallback((id, value, isValid) => {}, []);
+const NewArticle = () => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: "INPUT_CHANGE",
+      value: value,
+      isValid: isValid,
+      inputId: id,
+    });
+  }, []);
 
   return (
     <form className="article-form center">
@@ -20,7 +64,7 @@ const NewArticle = () => {
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a title."
-        onInput={inputTitleHandler}
+        onInput={inputHandler}
       />
       <Input
         id="description"
@@ -28,8 +72,11 @@ const NewArticle = () => {
         rows="4"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a description."
-        onInput={inputDescriptionHandler}
+        onInput={inputHandler}
       />
+      <Button type="submit" disabled={!formState.isValid}>
+        CREATE ARTICLE
+      </Button>
     </form>
   );
 };
