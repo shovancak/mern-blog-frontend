3f637c5fd40ff0df96eb87df42dcd0494148e7c6
shovancak <s.hovancak@gmail.com>
2020-08-27 14:40:12 +0200

    Using custom http hook for getting users

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -2,30 +2,21 @@ import React, { useEffect, useState } from "react";
 
 import UserList from "../components/UserList";
 import Card from "../../shared/components/Card";
+import { useHttpClient } from "../../shared/utilities/http-hook";
 
 const Users = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(undefined);
+  const { loading, error, request, clearError } = useHttpClient();
   const [loadedUsers, setLoadedUsers] = useState(undefined);
 
   useEffect(() => {
     const sendRequest = async () => {
-      setLoading(true);
       try {
-        const response = await fetch("http://localhost:5000/api/users");
-        const data = await response.json();
-        if (!response.ok) {
-          throw new Error(data.message);
-        }
+        const data = await request("http://localhost:5000/api/users");
         setLoadedUsers(data.users);
-        setLoading(false);
-      } catch (err) {
-        setError(err.message);
-      }
-      setLoading(false);
+      } catch (err) {}
     };
     sendRequest();
-  }, []);
+  }, [request]);
 
   return (
     <div>
