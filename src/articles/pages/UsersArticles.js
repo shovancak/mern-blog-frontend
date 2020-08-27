804dbee6267641010cc62899f343c362ebf1383b
shovancak <s.hovancak@gmail.com>
2020-08-27 17:12:23 +0200

    Adding request for getting articles by userID

--- src/articles/pages/UsersArticles.js
+++ src/articles/pages/UsersArticles.js
@@ -1,52 +1,36 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useHttpClient } from "../../shared/utilities/http-hook";
+import { AuthContext } from "../../shared/utilities/contex";
 
 import ArticleList from "./components/ArticleList";
+import Card from "../../shared/components/Card";
 
 import "./UsersArticles.css";
 
-const DUMMY_ARTICLES = [
-  {
-    id: "a1",
-    creator: "u1",
-    title: "MERN Stack",
-    imageUrl: "https://i.morioh.com/67feeaf72f.png",
-    description:
-      "One of the most popular technology stacks in web development.",
-    text:
-      "MERN Stack is a Javascript Stack that is used for easier and faster deployment of full-stack web applications. MERN Stack comprises of 4 technologies namely: MongoDB, Express, React and Node.js. It is designed to make the development process smoother and easier.",
-  },
-  {
-    id: "a2",
-    creator: "u2",
-    title: "React",
-    imageUrl: "https://probella.com/wp-content/uploads/2018/03/React-JS.png",
-    description:
-      "React JS is a JavaScript library used in web development to build interactive elements on websites.",
-    text:
-      "React is a JavaScript library that specializes in helping developers build user interfaces, or UIs.In terms of websites and web applications, UIs are the collection of on- screen menus, search bars, buttons, and anything else someone interacts with to USE a website or app.",
-  },
-  {
-    id: "a3",
-    creator: "u1",
-    title: "NodeJs",
-    imageUrl: "https://miro.medium.com/proxy/1*q9ww_u32hhpMaA-Q_s1ujw.png",
-    description:
-      "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.",
-    text:
-      "Node.js is a free, open-sourced, cross-platform JavaScript run-time environment that lets developers write command line tools and server-side scripts outside of a browser.",
-  },
-];
-
 const UsersArticles = () => {
+  const auth = useContext(AuthContext);
+  const { loading, error, request, clearError } = useHttpClient();
+  const [usersArticles, setUsersArticles] = useState(undefined);
+
   const userId = useParams().userId;
-  let usersPlaces = [];
-  DUMMY_ARTICLES.map((article) =>
-    article.creator === userId ? usersPlaces.push(article) : null
-  );
+
+  console.log(auth);
+  useEffect(() => {
+    const sendRequest = async () => {
+      try {
+        const data = await request(
+          `http://localhost:5000/api/articles/user/${userId}`
+        );
+        setUsersArticles(data.articles);
+      } catch (err) {}
+    };
+    sendRequest();
+  }, [request, userId]);
+
   return (
     <div className="articles">
-      <ArticleList items={usersPlaces} />
+      {!loading && usersArticles && <ArticleList items={usersArticles} />}
     </div>
   );
 };
