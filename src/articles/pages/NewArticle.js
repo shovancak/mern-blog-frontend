50b44e0239d03897add2ffc608951cdbd67709ec
shovancak <s.hovancak@gmail.com>
2020-08-27 16:04:29 +0200

    Creating POST request for creating new article

--- src/articles/pages/NewArticle.js
+++ src/articles/pages/NewArticle.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 
 import Input from "../../shared/components/Input";
 import Button from "../../shared/components/Button";
 import { VALIDATOR_REQUIRE } from "../../shared/utilities/validators";
 import { useForm } from "../../shared/utilities/form-validation-hook";
+import { useHttpClient } from "../../shared/utilities/http-hook";
+import { AuthContext } from "../../shared/utilities/contex";
 
 import "./NewArticle.css";
 
 const NewArticle = () => {
+  const auth = useContext(AuthContext);
+  const { loading, error, request, clearError } = useHttpClient();
+
   const [formState, inputHandler] = useForm(
     {
       title: {
@@ -30,9 +36,28 @@ const NewArticle = () => {
     false
   );
 
-  const articleSubmithandler = (event) => {
+  const history = useHistory();
+
+  const articleSubmithandler = async (event) => {
     event.preventDefault();
-    console.log(formState.inputs);
+    let title = formState.inputs.title.value;
+    try {
+      await request(
+        "http://localhost:5000/api/articles",
+        "POST",
+        JSON.stringify({
+          creator: auth.userId,
+          description: formState.inputs.description.value,
+          imageUrl: formState.inputs.imageUrl.value,
+          text: formState.inputs.text.value,
+          title: title,
+        }),
+        {
+          "Content-Type": "application/json",
+        }
+      );
+      history.push("/");
+    } catch (err) {}
   };
 
   return (
