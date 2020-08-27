ea129c5867425d2bf16c165ccce47a4d42e65a26
shovancak <s.hovancak@gmail.com>
2020-08-27 14:05:15 +0200

    Creating custom http hook from requests

---
+++ src/shared/utilities/http-hook.js
@@ -0,0 +1,47 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+
+export const useHttpClient = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const activeRequest = useRef([]);
+
+  const request = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setLoading(true);
+      const httpAbortController = new AbortController();
+      activeRequest.current.push(httpAbortController);
+      try {
+        const response = await fetch(url, {
+          method: method,
+          body: body,
+          headers: headers,
+          signal: httpAbortController.signal,
+        });
+
+        const data = await response.json();
+        // if response code is 4xx or 5xx error will be thrown
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+        return data;
+      } catch (err) {
+        setError(err.message || "Something went wrong, please try again.");
+      }
+      setLoading(false);
+    },
+    []
+  );
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      activeRequest.current.forEach((abortController) =>
+        abortController.abort()
+      );
+    };
+  }, []);
+  return { loading, error, request, clearError };
+};
